f6e2248a2c23248ed690b659922c3d92ad5adcf6
modest-hijab <ayah1328@aol.com>
2025-01-20 02:40:07 -0500

    Finished assignment

--- js/assignment.js
+++ js/assignment.js
@@ -7,31 +7,33 @@ const createNumberArrayElement = document.getElementById("create-number-array");
 
 function evenOrOdd() {
     const num = 3;
-    // Write the logic to decide if the variable "num" is even or odd
-    // and set the element's value the string "Even" or "Odd" accordingly
-
+    if (num % 2 === 0) {
+        evenOrOddElement.textContent = "Even";
+    } else {
+        evenOrOddElement.textContent = "Odd";
+    }
 }
 
 function sumTheNumbers() {
     let sum = 0;
-    // Write the logic to sum the numbers 1 through 10
-    // using a for loop. Check the expected output
-    // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        sum += i;
+    }
+    sumTheNumbersElement.textContent = sum;
 }
 
 function createNumberArray() {
     const numberArray = [];
-
-    // Write the logic that loops 10 times and adds the value
-    // to numberArray each iteration. Check the expected output
-    // on the assignment page
-
+    for (let i = 1; i <= 10; i++) {
+        numberArray.push(i);
+    }
+    createNumberArrayElement.textContent = numberArray.join(",");
 }
 
 function render() {
-    // Call the created functions
-
+    evenOrOdd();
+    sumTheNumbers();
+    createNumberArray();
 }
 
 submissionBtn.addEventListener("click", function () {
--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/* Break example
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break;
+    }
+    console.log("Break example, i:", i);
+}
+
+// Continue example
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log("Continue example, i:", i);
+}
+*/
